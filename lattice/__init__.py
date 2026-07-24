4ac225a2df9266ab4dcb565cd45ace5d458f8031
NathanPunya <nathanpunyataweekul@gmail.com>
2026-07-23 17:28:09 -0700

    Export public matching-engine types from the package.

---
+++ lattice/__init__.py
@@ -0,0 +1,27 @@
+from lattice.book import OrderBook
+from lattice.types import (
+    BookLevel,
+    BookSnapshot,
+    EventKind,
+    Fill,
+    Liquidity,
+    Order,
+    Side,
+    TickSize,
+    TIF,
+    Trade,
+)
+
+__all__ = [
+    "OrderBook",
+    "BookLevel",
+    "BookSnapshot",
+    "EventKind",
+    "Fill",
+    "Liquidity",
+    "Order",
+    "Side",
+    "TickSize",
+    "TIF",
+    "Trade",
+]
